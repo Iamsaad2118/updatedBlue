f22e1dcdf91eadcb7f2a48d3ad5490e15c184c10
Jordan <jsilk@vin2.com>
2023-11-15 07:47:33 +0500

    feat(wagmi):init setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
+"use client"
 import { SectionRoadMap } from "./page-sections/road-map/SectionRoadMap";
 import { SectionSave } from "./page-sections/save/SectionSave";
 import { SectionToken } from "./page-sections/token/SectionToken";
 import { SectionHero } from "./page-sections/hero/SectionHero";
+import { WagmiConfig } from 'wagmi'
+import { config } from "@/constants/WagmiConfig";
 
 export default function Home() {
   return (
     <main className="py-24 bg-neutral-800">
-      <SectionHero />
-      <SectionSave />
-      <SectionToken />
-      <SectionRoadMap />
+      <WagmiConfig config={config}>
+        <SectionHero />
+        <SectionSave />
+        <SectionToken />
+        <SectionRoadMap />
+      </WagmiConfig>
     </main>
   );
 }
---
+++ src/constants/WagmiConfig.ts
@@ -0,0 +1,15 @@
+import { configureChains, mainnet, createConfig } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [publicProvider()],
+)
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+})
+
+export { config }
