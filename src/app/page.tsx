04aee7f3d8adf5fcdbc0ce86b2df91f025bbc16b
Jordan <jsilk@vin2.com>
2023-11-16 09:55:39 +0500

    feat(WC): Rainbow kit, handle connection, support Metamask, Coinbase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
-"use client"
+"use client";
+import "@rainbow-me/rainbowkit/styles.css";
 import { SectionRoadMap } from "./page-sections/road-map/SectionRoadMap";
 import { SectionSave } from "./page-sections/save/SectionSave";
 import { SectionToken } from "./page-sections/token/SectionToken";
 import { SectionHero } from "./page-sections/hero/SectionHero";
-import { WagmiConfig } from 'wagmi'
-import { config } from "@/constants/WagmiConfig";
+import { WagmiConfig } from "wagmi";
+import { chains, config } from "@/constants/WagmiConfig";
+import AppHeaderContent from "@/components/app-header";
+import { Footer } from "@/components/footer/Footer";
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
   return (
-    <main className="py-24 bg-dark">
+    <main className="bg-dark">
       <WagmiConfig config={config}>
-        <SectionHero />
-        <SectionSave />
-        <SectionToken />
-        <SectionRoadMap />
+        <RainbowKitProvider
+          theme={darkTheme({ accentColor: "#55AECC" })}
+          chains={chains}
+        >
+          <AppHeaderContent />
+          <SectionHero />
+          <SectionSave />
+          <SectionToken />
+          <SectionRoadMap />
+          <Footer />
+        </RainbowKitProvider>
       </WagmiConfig>
     </main>
   );
--- src/constants/WagmiConfig.ts
+++ src/constants/WagmiConfig.ts
@@ -1,15 +1,23 @@
-import { configureChains, mainnet, createConfig } from 'wagmi'
-import { publicProvider } from 'wagmi/providers/public'
+import { configureChains, mainnet, createConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet],
   [publicProvider()],
-)
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Hamaca",
+  projectId: "04b06c471b88e576c1a3d5b9e69d3827",
+  chains,
+});
 
 const config = createConfig({
   autoConnect: true,
   publicClient,
   webSocketPublicClient,
-})
+  connectors,
+});
 
-export { config }
+export { config, connectors, chains };
